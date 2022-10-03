6bb4a1df5f919662a66d385aec3d7d85385bc632
T4NVESH <101313936+T4NVESH@users.noreply.github.com>
2022-10-03 23:09:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const getquote = async () => {
+    try{
+      const res = fetch(https://parade.com/973277/jessicasager/inspirational-quotes/); 
+    }catch(console.error();) {}
+}
+
+
+window.addEventListener("load",() => {
+    getquote();
+});
